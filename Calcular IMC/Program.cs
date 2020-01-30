2d0cda02e5f371370e69cd926114b58692039eca
José Doñe <josedoneguillen1992@hotmail.com>
2020-01-30 00:39:58 -0400

    Add project files.

---
+++ Calcular IMC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calcular_IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular IMC");
+        }
+    }
+}
