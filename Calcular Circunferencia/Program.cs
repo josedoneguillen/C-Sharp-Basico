ccb605aee7ed19f019d80e6a1db69e4e3940e02d
José Doñe <josedoneguillen1992@hotmail.com>
2020-01-30 00:47:29 -0400

    Nueva estructura de carpetas

---
+++ Calcular Circunferencia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calcular_Circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular Circunferencia");
+        }
+    }
+}
---
+++ Radio Circunferencia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Radio_Circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular Radio de una circunferencia");
+        }
+    }
+}
