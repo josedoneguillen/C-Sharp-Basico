48c182ea0107c4f7e8aa5b6e3e3185d3d8dedbc2
José Doñe <josedoneguillen1992@hotmail.com>
2020-01-31 00:03:38 -0400

    Inicializacion de programa signo zodiacal y reestructuracion de carpeta para que este dentro de operadores y condicionales.

---
+++ Operadores y Condicionales/Ecuaciones de 2do Grado/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ecuaciones_de_2do_Grado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4- Crear un programa que resuelva ecuaciones de segundo grados por la fórmula general para calcular raíz cuadrada en c# se utiliza la clase Math y el método sqrt , (Math.sqrt) referencia https://matematicascercanas.com/2016/11/01/formula-ecuaciones-polinomicas-segundo-grado/  */
+            Console.WriteLine("resuelva ecuaciones de segundo grados por la fórmula general");
+        }
+    }
+}
