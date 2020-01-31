395e9073dfb78b2afaa58b04371cdef49675ec86
José Doñe <josedoneguillen1992@hotmail.com>
2020-01-30 23:53:28 -0400

    Inicialización programa signo zodiacal.

---
+++ Signo Zodiacal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Signo_Zodiacal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 3- Crear un programa que te diga tu signo zodiacal según tu día y mes de nacimiento. */
+            Console.WriteLine("Signo zodiacal según tu dia y mes de nacimiento");
+        }
+    }
+}
