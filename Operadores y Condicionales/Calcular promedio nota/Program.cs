4eaabf1ec35f9df35e440fb12b924d393e08edb4
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-01 01:26:30 -0400

    Inicializacion del proyecto para calcular promedio de 4 numeros literalmente.

---
+++ Operadores y Condicionales/Calcular promedio nota/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calcular_promedio_nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             6 realizar un programa que calcule el promedio de 4 números y si el resultado es mayor de 70
+             mostrarme la literal que obtuve siendo A >= 90 y <= 100 , B >= 80 y <= 89 y C >= 70 y <= 79, 
+             de sacar menos de 70 debe decirme que he reprobado.
+             */
+            Console.WriteLine("Calcular promedio nota");
+        }
+    }
+}
