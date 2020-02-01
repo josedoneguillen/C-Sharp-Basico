5f1c7c4eb697bc6ba3aefed71aa5ff4e9b18375d
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-01 15:01:51 -0400

    Inicializacion de proyecto pragrama analizador de generacion.

---
+++ Operadores y Condicionales/Analizador de Generacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Analizador_de_Generacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                8- realizar una aplicación que según mi año de nacimiento me diga
+                a cuál generación pertenezco (referencia https://www.lavanguardia.com/vivo/20180408/442342457884/descubre-que-generacion-perteneces.html ) 
+            */
+            Console.WriteLine("Analizador de Generación");
+        }
+    }
+}
