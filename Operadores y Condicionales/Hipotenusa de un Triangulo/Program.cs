35e3680c60d002ba1e9008b4372a21689d419ab0
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-01 16:52:04 -0400

    Programa para calcular la hipotenusa de un triángulo rectángulo terminado.

--- Operadores y Condicionales/Hipotenusa de un Triangulo/Program.cs
+++ Operadores y Condicionales/Hipotenusa de un Triangulo/Program.cs
@@ -11,6 +11,25 @@ namespace Hipotenusa_de_un_Triangulo
                 referencia https://www.universoformulas.com/matematicas/geometria/hipotenusa/ 
             */
             Console.WriteLine("Calcular la hipotenusa de un triángulo rectángulo");
+
+            /* Declaración de variables */
+            double a, b, h;
+
+            /* Solicitar al usuario los valores de a y b */
+            Console.WriteLine("Ingrese el valor de a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de b: ");
+            b = double.Parse(Console.ReadLine());
+
+            /* Realizar cálculo */
+            h = Math.Sqrt( ( Math.Pow(a, 2) ) + ( Math.Pow(b,2) ) );
+
+            /* Imprimir el resultado */
+            Console.WriteLine("la hipotenusa del triángulo rectángulo es: " + h);
+
+            //Esperar una tecla para cerrar
+            Console.ReadKey();
         }
     }
 }
