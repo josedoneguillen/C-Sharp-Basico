79f7b824d9d7c524124c729613a3707bca9d2ce4
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-06 00:19:06 -0400

    Creando carpeta blucles y inicializando proyecto de patron diamante

---
+++ Bucles/patron_de_diamante/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace patron_de_diamante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Escriba un programa en C # para mostrar un patrón de diamante: */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
