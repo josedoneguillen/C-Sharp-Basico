89eeb9a2277b9135280354f425c94f474b5f2d93
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-06 15:10:04 -0400

    Inicializando proyecto numeros impares

---
+++ Bucles/numeros_impares/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace numeros_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                2. Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
+                todos los números impares desde 1 hasta ese número separados por comas.
+             */
+            Console.WriteLine("Imprime numeros impares");
+
+            /* Esperar letra para cerrar */
+            Console.ReadKey();
+        }
+    }
+}
