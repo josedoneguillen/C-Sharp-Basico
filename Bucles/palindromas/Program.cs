0fcce754eacbec50a282221bd83628d5c6eee060
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-07 14:01:21 -0400

    Proyecto palabras palindromas terminado

---
+++ Bucles/palindromas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace palindromas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 10.Escribir un programa que diga si dos palabras son palindromas.
+             */
+
+            /* Declaracion de variables */
+            string palabra, invertida = "";
+
+            Console.WriteLine("Validar palabras palindromas, Ej: OSO");
+
+            /* Solicitar al usuario ingrsar una palabra */
+            Console.WriteLine("Ingrese una palabra: ");
+            palabra = Console.ReadLine();
+
+            /* Ciclo for para recorrer los caracteres de la palabra */
+            for (int a = (palabra.Length - 1); a >= 0; a--) 
+            {
+                invertida += palabra[a];
+            }
+
+            /* Condicional para comparar resultado */
+            if (palabra == invertida)
+            {
+                Console.WriteLine("Esta palabra es palindroma! =D ");
+            }
+            else 
+            {
+                Console.WriteLine("Esta palabra NO es palindroma =( ");
+            }
+
+            /* Esperar letra para cerrar */
+            Console.WriteLine("Pulse una tecla para cerrar el programa.");
+            Console.ReadKey();
+        }
+    }
+}
