7e552d9a242f060df57af0a4f89c68e9901e5d49
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-06 21:35:32 -0400

    Agregando separador por comas a la iteracion de numeros impares

--- Bucles/numeros_impares/Program.cs
+++ Bucles/numeros_impares/Program.cs
@@ -16,14 +16,24 @@ namespace numeros_impares
             int n;
 
             /* Solicitar al usuario el numero hasta el cual iterar */
-            Console.WriteLine("Ingrese un numero");
+            Console.WriteLine("Ingrese un numero:");
             n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
             /* Ciclo for para contar hasta el numero ingresado por el usuario */
             for (int i = 1; i <= n; i+=2) 
             {
-                    Console.WriteLine(i);
+                /* Separar por coma despues del primer elemento */
+                if (i > 1) 
+                {
+                    Console.Write(", ");
+                }
+
+                Console.Write(i);
+
             }
+
+            /* Creando un salto de linea despues del for */
+            Console.WriteLine(Environment.NewLine);
 
             /* Esperar letra para cerrar */
             Console.WriteLine("Pulse una tecla para cerrar el programa.");
