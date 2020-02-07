15cd951ec24d3baef317edf550909bf5c0a846d4
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-07 00:27:17 -0400

    Proyecto terminos serie de fibonacci completada

--- Bucles/terminos_de_fibonacci/Program.cs
+++ Bucles/terminos_de_fibonacci/Program.cs
@@ -6,7 +6,34 @@ namespace terminos_de_fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* 
+             * 7. Escriba un programa para imprimir series de n términos de Fibonacci 
+             * donde n es ingresado por el usuario a través del teclado: 0 1 1 2 3 5 8 13 24 
+             */
+            Console.WriteLine("series de n términos de Fibonacci");
+            /* Declaracion de variables */
+            int n, a = 0, b = 1, t;
+
+            /* Solicitar al usuario el numero hasta el cual iterar la serie de fibonacci */
+            Console.WriteLine("Ingrese un numero:");
+            n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            /* Ciclo for para iterar la serie de fibonacci n veces */
+            for (int i = 0; i < n; i++) {
+
+                /* Imprimir numero de la serie */
+                Console.WriteLine(a);
+
+                /* Sumar numero de la serie para optener la sucesion de fibonaci para la siguiente iteracion */
+                t = a + b;
+                a = b;
+                b = t;
+
+            }
+
+            /* Esperar letra para cerrar */
+            Console.WriteLine("Pulse una tecla para cerrar el programa.");
+            Console.ReadKey();
         }
     }
 }
