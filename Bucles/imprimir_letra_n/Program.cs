ffefcc50521ab9b9f61c6d7091ef6e4538de5563
José Doñe <josedoneguillen1992@hotmail.com>
2020-02-07 14:42:58 -0400

    Programa imprimir letra N con asteriscos terminado

---
+++ Bucles/imprimir_letra_n/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace imprimir_letra_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 11.Escriba un programa  para mostrar un patrón en forma de N con asterisco.
+             */
+
+            Console.WriteLine("patrón en forma de N con asterisco");
+
+            /* Filas */
+            for (int a = 1; a <= 7; a++) 
+            {
+                /* Columnas */
+                for (int b = 1; b <= 5; b++)
+                {
+                    /* Determinar espacios y asteriscos segun posicion */
+                    if (b > 1 && b < 5)
+                    {
+                        
+                        if (a > 2 && a < 6)
+                        {
+                            if (a == 3 && b > 2)
+                            {
+                                Console.Write(" ");
+                            }
+                            else if ((a == 4 && b == 2) || (a == 4 && b == 4))
+                            {
+                                Console.Write(" ");
+                            }
+                            else if ((a == 5 && b > 1 && b < 4))
+                            {
+                                Console.Write(" ");
+                            }
+                            else
+                            {
+                                Console.Write("*");
+                            }
+
+                        }
+                        else 
+                        {
+                            Console.Write(" ");
+                        }
+
+                    }
+                    else 
+                    {
+                        Console.Write("*");
+                    }
+
+                }
+
+                /* Salto de linea al final de cada fila */
+                Console.Write(Environment.NewLine);
+
+            }
+
+            /* Esperar letra para cerrar */
+            Console.WriteLine("Pulse una tecla para cerrar el programa.");
+            Console.ReadKey();
+        }
+    }
+}
