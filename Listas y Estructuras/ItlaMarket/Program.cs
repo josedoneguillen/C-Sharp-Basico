683c70e8c5879827fe83dfe9fe17664d1d2979d2
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-02 16:05:14 -0400

    Terminada area de clientes y ahora trabajando area de productos

--- Listas y Estructuras/ItlaMarket/Program.cs
+++ Listas y Estructuras/ItlaMarket/Program.cs
@@ -117,6 +117,10 @@ namespace ItlaMarket
 
         }
 
+        /*
+         * Metodos de clientes
+         */
+
         /* Metodo para imprimir el menu de clientes */
         public static void menuClientes()
         {
@@ -145,21 +149,37 @@ namespace ItlaMarket
 
                     agregarCliente();
 
+                    //Al terminar volver al menu de clientes
+                    menuClientes();
+
                     break;
 
                 /* Editar Cliente */
                 case 2:
                     editarCliente();
+
+                    //Al terminar volver al menu de clientes
+                    menuClientes();
+
                     break;
 
                 /* Borrar Cliente */
                 case 3:
+
+                    borrarCliente();
+
+                    //Al terminar volver al menu de clientes
+                    menuClientes();
 
                     break;
 
                 /* Listar Clientes */
                 case 4:
                     listarClientes();
+
+                    //Al terminar volver al menu de clientes
+                    menuClientes();
+
                     break;
 
                 /* Atras */
@@ -186,6 +206,9 @@ namespace ItlaMarket
             /* Solicitar al usuario el nombre del nuevo cliente */
             Console.WriteLine("Ingrese el nombre del nuevo cliente: ");
             clientes.Add(Console.ReadLine());
+
+            //Al terminar volver al menu de clientes
+            menuClientes();
         }
 
         /* Metodo para listar clientes */
@@ -200,6 +223,10 @@ namespace ItlaMarket
             foreach (string cliente in clientes)
             {
                 Console.WriteLine(i + "- " + cliente);
+
+                //Incrementar contador
+                i++;
+
             }
 
             if (esperar)
@@ -252,21 +279,209 @@ namespace ItlaMarket
 
                 listarClientes(false);
 
-                /* Solicitar al usuario el indice del cliente a modificar */
+                /* Solicitar al usuario el indice del cliente a borrar */
                 Console.WriteLine("Ingrese el indice del cliente: ");
 
                 int cliente = Convert.ToInt32(Console.ReadLine());
 
-                /* Solicitar al usuario el nuevo nombre del cliente*/
-                Console.WriteLine("Ingrese el nuevo nombre del cliente: ");
-
-                clientes[(cliente - 1)] = Console.ReadLine();
+                /* Eliminar el cliente */
+                clientes.RemoveAt(cliente - 1);
 
             }
             else
             {
                 //Imprimir mensaje si no hay clientes
                 Console.WriteLine("No hay clientes");
+                Console.ReadKey();
+            }
+
+        }
+
+        /*
+         * Metodos de productos
+         */
+
+        /* Metodo para imprimir el menu de producto */
+        public static void menuProductos()
+        {
+            Console.Clear();
+            Console.WriteLine("Punto de venta - Productos");
+            Console.WriteLine("1- Agregar Producto");
+            Console.WriteLine("2- Editar Producto");
+            Console.WriteLine("3- Borrar Producto");
+            Console.WriteLine("4- Listar Productos");
+            Console.WriteLine("5- Atras");
+
+            //Declaracion de variable opcion
+            int opcion;
+
+            //Solicitar al usuario una opcion
+            Console.WriteLine("Ingrese una de las opciones:");
+
+            /* Almacenar opcion */
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            /* Sentencia switch para evaluar la opcion escogida por el usuario */
+            switch (opcion)
+            {
+                /* Agregar Producto */
+                case 1:
+
+                    agregarProducto();
+
+                    //Al terminar volver al menu de Productos
+                    menuProductos();
+
+                    break;
+
+                /* Editar Producto */
+                case 2:
+                    editarProducto();
+
+                    //Al terminar volver al menu de Productos
+                    menuProductos();
+
+                    break;
+
+                /* Borrar Producto */
+                case 3:
+
+                    borrarProducto();
+
+                    //Al terminar volver al menu de Productos
+                    menuProductos();
+
+                    break;
+
+                /* Listar Productos */
+                case 4:
+                    listarProductos();
+
+                    //Al terminar volver al menu de Productos
+                    menuProductos();
+
+                    break;
+
+                /* Atras */
+                case 5:
+                    menuPrincipal();
+                    break;
+
+                /* Defecto */
+                default:
+                    Console.WriteLine("La opcion elegida no existe!");
+                    break;
+            }
+
+
+
+
+        }
+
+        /* Metodo para agregar productos */
+        public static void agregarProducto()
+        {
+            Console.Clear();
+
+            /* Solicitar al usuario el nombre del nuevo Producto */
+            Console.WriteLine("Ingrese el nombre del nuevo producto: ");
+            string nombre = Console.ReadLine();
+
+            /* Solicitar al usuario el precio del nuevo Producto */
+            Console.WriteLine("Ingrese el precio del nuevo producto: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            /* Solicitar al usuario la cantidad del nuevo Producto */
+            Console.WriteLine("Ingrese la cantidad del nuevo producto: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            inventario.Add(new producto { nombre = nombre, precio = precio, cantidad = cantidad });
+
+            //Al terminar volver al menu de Productos
+            menuProductos();
+        }
+
+        /* Metodo para listar productos */
+        public static void listarProductos(bool esperar = true)
+        {
+            Console.Clear();
+
+            /* Listar producto */
+            Console.WriteLine("Listado de productos: ");
+            int i = 1;
+
+            foreach (producto producto in inventario)
+            {
+                Console.WriteLine(i + "- " + producto);
+
+                //Incrementar contador
+                i++;
+
+            }
+
+            if (esperar)
+            {
+                /* Esperar letra para volver atras */
+                Console.WriteLine("Pulse una tecla para volver atras.");
+                Console.ReadKey();
+            }
+        }
+
+        /* Metodo para editar productos */
+        public static void editarProducto()
+        {
+            Console.Clear();
+
+            //Sentencia if para comprobar si hay productos
+            if (inventario.Count > 0)
+            {
+
+                listarClientes(false);
+
+                /* Solicitar al usuario el indice del producto a modificar */
+                Console.WriteLine("Ingrese el indice del producto: ");
+
+                int producto = Convert.ToInt32(Console.ReadLine());
+
+                /* Solicitar al usuario el nuevo nombre del producto */
+                Console.WriteLine("Ingrese el nuevo nombre del producto: ");
+
+                string nombre = Console.ReadLine();
+
+            }
+            else
+            {
+                //Imprimir mensaje si no hay productos
+                Console.WriteLine("No hay productos");
+                Console.ReadKey();
+            }
+
+        }
+
+        /* Metodo para borrar productos */
+        public static void borrarProducto()
+        {
+            Console.Clear();
+
+            //Sentencia if para comprobar si hay productos
+            if (inventario.Count > 0)
+            {
+
+                listarProductos(false);
+
+                /* Solicitar al usuario el indice del cliente a borrar */
+                Console.WriteLine("Ingrese el indice del producto: ");
+
+                int producto = Convert.ToInt32(Console.ReadLine());
+
+                /* Eliminar el producto */
+                clientes.RemoveAt(producto - 1);
+
+            }
+            else
+            {
+                //Imprimir mensaje si no hay productos
+                Console.WriteLine("No hay productos");
                 Console.ReadKey();
             }
 
